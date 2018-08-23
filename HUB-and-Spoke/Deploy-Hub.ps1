38aeef171334a705878689056a09d1eb2b58e9b7
Victor Dantas Mehmeri <v.dantas.mehmeri@accenture.com>
2018-08-23 14:38:48 +0200

    Major refactoring and fixes

--- HUB-and-Spoke/Deploy-Hub.ps1
+++ HUB-and-Spoke/Deploy-Hub.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'rg-hub',
+    [string] $ResourceGroupName = 'RG-HUB',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
